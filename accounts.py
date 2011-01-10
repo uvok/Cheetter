e4d8b27e5bff52d3d0754ad5dbb2a84551573321
uvok <uvok@online.de>
2011-01-10 22:06:38 +0100

    Perform actual check if accounts exist on init, ad account

--- accounts.py
+++ accounts.py
@@ -5,13 +5,24 @@ import sql
 class AccountManager( object ):
     def __init__(self):
         self.db=sql.liteDB()
+        self.result=None
         self._NoAccounts()
     def __del__(self):
         del self.db
     def _NoAccounts(self):
         if self.db.howmany()==0:
-            print "You have no acounts configured yet!" \
-                  "Would you like to create one now? "
-            ## TODO
-    
+            while True:
+                print "You have no acounts configured yet!\n" \
+                      "Would you like to create one now? "
+                answ=raw_input()
 
+                if answ.lower()=='y':
+                    self.AddAccount()
+                    break
+                elif answ.lower()=='n':
+                    print "Using client not possible w/o account, quitting"
+                    exit()
+                else:
+                    print "Invalid answer"
+                    pass
+            
